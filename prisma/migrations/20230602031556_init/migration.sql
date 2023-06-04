0338ab1978bdfdf02db65b39b3f559a4804b41c6
Nguyen Hoang Long <long.nguyen2706@hcmut.edu.vn>
2023-06-04 13:15:30 +0700

    fix: remove prisma migrations

--- prisma/migrations/20230602031556_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateEnum
-CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'USER');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "emailVerified" TIMESTAMP(3),
-ADD COLUMN     "image" TEXT,
-ADD COLUMN     "role" "UserRole" NOT NULL DEFAULT 'USER';
