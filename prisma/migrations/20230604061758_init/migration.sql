f8c3e20e7bd6dfbac26586fde078f74c6d887527
Nguyen Hoang Long <long.nguyen2706@hcmut.edu.vn>
2023-06-04 13:15:30 +0700

    fix: remove prisma migrations
    fix: remove prisma migrations

--- prisma/migrations/20230604061758_init/migration.sql
+++ prisma/migrations/20230604061758_init/migration.sql
@@ -1,9 +1,15 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'USER');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "name" TEXT,
     "email" TEXT NOT NULL,
+    "emailVerified" BOOLEAN,
+    "image" TEXT,
     "password" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
