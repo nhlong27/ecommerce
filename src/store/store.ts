70c1d8d202f7a50702eec685bd05495a3c62d127
Nguyen Hoang Long <long.nguyen2706@hcmut.edu.vn>
2023-08-14 15:19:50 +0700

    fix: remove past redux implementation

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,13 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import accountSectionSlice from './slices/accountSectionSlice';
 
 export const createStore = () =>
- configureStore({
+  configureStore({
     reducer: {
       accountSectionReducer: accountSectionSlice,
     },
   });
 
-
-  export const store = createStore();
-  export type RootState = ReturnType<typeof store.getState>;
-  export type AppDispatch = ReturnType<typeof store.dispatch>;
+export const store = createStore();
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = ReturnType<typeof store.dispatch>;
